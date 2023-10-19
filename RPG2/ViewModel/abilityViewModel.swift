3268dc202dc30c6e3d4eaff487517c743adec447
Christopher <Christopher.woods723@gmail.com>
2023-10-19 13:16:03 -0400

    added common element images and abilities

--- RPG2/ViewModel/abilityViewModel.swift
+++ RPG2/ViewModel/abilityViewModel.swift
@@ -12,7 +12,10 @@ class abilityViewModel: ObservableObject{
     
     @Published var abilities: [Abilities] = [
         Abilities(element: "Fire", abilities: ["Fireball", "Firetornado", "SunExplosion"], description: ["Throw a Fireball", "Create tornado of fire", "Explode the sun"]),
-        Abilities(element: "Water", abilities: ["Waterblast", "Whirlpool", "Greatflood"], description: ["Throw a Fireball", "Create tornado of fire", "Explode the sun"])
+        Abilities(element: "Water", abilities: ["Waterblast", "Whirlpool", "Greatflood"], description: ["Throw a Waterblast", "Create tornado of fire", "Explode the sun"]),
+        Abilities(element: "Earth", abilities: ["Earthblast", "Rockslide", "Earthquake"], description: ["Throw a Rock", "Create tornado of fire", "Explode the sun"]),
+        Abilities(element: "Air", abilities: ["Fly", "Aircutter", "Tornado"], description: ["Throw a Rock", "Create tornado of fire", "Explode the sun"]),
+        
     ]
     
     
